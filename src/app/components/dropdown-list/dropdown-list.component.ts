a3f001e099044ada7799197dfb42a0ed8903186e
Oleksii_Bielogoloviy <Oleksii_Bielogoloviy@epam.com>
2024-09-04 12:57:04 +0300

    Fix notes

--- src/app/components/dropdown-list/dropdown-list.component.ts
+++ src/app/components/dropdown-list/dropdown-list.component.ts
@@ -1,21 +1,29 @@
 import { Component, inject } from '@angular/core';
 import { CurrencyDataService } from '../../services/currency-data.service';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-dropdown-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './dropdown-list.component.html',
   styleUrl: './dropdown-list.component.scss'
 })
 export class DropdownListComponent {
 
-  service: CurrencyDataService = inject(CurrencyDataService); 
-  currencyData: Array<string> = this.service.getCurrencies()
+  private service: CurrencyDataService
+  public currencyData: Array<string>
+
+  constructor() {
+    this.service = inject(CurrencyDataService)
+    this.currencyData = this.service.getCurrencies()
+  }
 
 
-
-  onSelected(value:string): void {
-		this.service.setSelectedCurrency(value);
-	}
+  onChange(event: Event): void {
+    const inputElement = event.target as HTMLInputElement;
+    const selectedValue = inputElement.value;
+    this.service.setSelectedCurrency(selectedValue);
+    
+  }
 }
